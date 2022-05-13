d8a12b28184747c028c3c8eb38e001d0ba39ffb3
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2022-05-13 17:36:06 +0300

    Fixed bugs, added icons, autoprefixer

--- components/MuteButton.js
+++ components/MuteButton.js
@@ -4,21 +4,25 @@ import Tooltip from "@mui/material/Tooltip";
 import Fab from "@mui/material/Fab";
 import { useState } from "react";
 import styles from "../styles/MuteButton.module.css";
+import Fade from "@mui/material/Fade";
+
 export default function MuteButton({ callback }) {
   const [muted, setMuted] = useState(false);
   return (
-    <Tooltip title={muted ? "Unmute" : "Mute"}>
-      <Fab
-        onClick={() => {
-          callback();
-          setMuted(prev => !prev);
-        }}
-        color='secondary'
-        aria-label='mute'
-        className={styles.muteButton}
-      >
-        {muted ? <VolumeOffIcon /> : <VolumeUpIcon />}
-      </Fab>
-    </Tooltip>
+    <Fade in={true} timeout={1000}>
+      <Tooltip title={muted ? "Unmute" : "Mute"}>
+        <Fab
+          onClick={() => {
+            callback();
+            setMuted(prev => !prev);
+          }}
+          color='secondary'
+          aria-label='mute'
+          className={styles.muteButton}
+        >
+          {muted ? <VolumeOffIcon /> : <VolumeUpIcon />}
+        </Fab>
+      </Tooltip>
+    </Fade>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,81 @@ export default function Home() {
           name='viewport'
           content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
         />
+        {/* to show icon on firefox for android etc (chrome find the favicon automatically) */}
+        {/* <link rel='icon' href='/favicon.ico?v=2' /> */}
+        <link
+          rel='apple-touch-icon'
+          sizes='57x57'
+          href='/apple-icon-57x57.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='60x60'
+          href='/apple-icon-60x60.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='72x72'
+          href='/apple-icon-72x72.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='76x76'
+          href='/apple-icon-76x76.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='114x114'
+          href='/apple-icon-114x114.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='120x120'
+          href='/apple-icon-120x120.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='144x144'
+          href='/apple-icon-144x144.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='152x152'
+          href='/apple-icon-152x152.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-icon-180x180.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='192x192'
+          href='/android-icon-192x192.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='96x96'
+          href='/favicon-96x96.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/manifest.json' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
+        <meta name='theme-color' content='#ffffff' />
       </Head>
       <Score score={score} />
       <Question setAnswer={e => (answer.current = e)} score={score} />
@@ -27,6 +102,12 @@ export default function Home() {
       <MuteButton
         callback={() => {
           isSound.current = !isSound.current;
+          if (!isSound.current) {
+            // if the sound is muted, pause all sounds before deleting them (otherwise, the sound will continue to play)
+            audios.current.click && audios.current.click.pause();
+            audios.current.bad && audios.current.bad.pause();
+            audios.current.bitGood && audios.current.bitGood.pause();
+          }
           setSound();
           isSound.current &&
             audios.current.click &&
