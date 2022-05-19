8d778a0a9e9eb414ac3983851439a67176901b47
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2022-05-19 21:42:54 +0300

    Removed Quiz.js, it has no state anymore

---
+++ hooks/useQuiz.js
@@ -0,0 +1,50 @@
+import { useRef, useEffect } from "react";
+
+export default function useQuiz(
+  score,
+  status,
+  audios,
+  setStatus,
+  setScore,
+  time
+) {
+  const timeRef = useRef(null); // to store the setTimeout reference, so we can clear it when the user clicks on an answer
+  const wrongAnswerRef = useRef(null); // to store the wrong answer (int)
+  const answer = useRef(0); // the correct answer (int)
+
+  function handleClick(num) {
+    // this function will be called when the user clicks on an answer
+    setStatus(1); // set the status to "playing"
+    if (answer.current === num) {
+      // setScore(prev => (prev < 10 ? prev + 1 : prev + 2)); // get 2 points if the question is hard...
+      setScore(prev => prev + 1); // if the answer is correct, increase the score only by 1
+      audios.current.bitGood && audios.current.bitGood.play();
+    } else {
+      wrongAnswerRef.current = num;
+      audios.current.bad && audios.current.bad.play();
+      setStatus(2);
+    }
+  }
+
+  useEffect(() => {
+    // this useEffect handles the time interval
+
+    if (timeRef.current) {
+      clearTimeout(timeRef.current);
+    }
+
+    if (status === 1) {
+      // if the user started the game (score: 1+) and the game is still playing, we need to set a new time interval
+      timeRef.current = setTimeout(() => {
+        audios.current.bad && audios.current.bad.play();
+        setStatus(2);
+      }, time.current * 1000 + 200);
+    }
+  }, [score, status]); // this will run only when score or playing changes (remove timer, and set new one if still playing)
+
+  return {
+    handleClick,
+    wrongAnswerRef,
+    answer,
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,59 @@
 import { useState, useRef } from "react";
-import Quiz from "../components/Quiz";
 import Score from "../components/Score";
 import Question from "../components/Question";
 // import styles from "../styles/Home.module.css";
 import useSound from "../hooks/useSound";
 import MyHead from "../components/MyHead";
 import Fabs from "../components/Fabs";
+import Answers from "../components/Answers";
+import GameOver from "../components/GameOver";
+import useQuiz from "../hooks/useQuiz";
 
 export default function Home() {
   // TODO:
   // 1. fix Firefox bug
   // 2. fix audio re-fetching
-  // 3. remove Quiz.js, it has no independent state
 
   const [score, setScore] = useState(0);
-  const answer = useRef(0); // the correct answer (int)
   const [audios, handleMute] = useSound(); // all the audio logic
   const timeLimit = useRef(5); // time limit (int)
   const [status, setStatus] = useState(0); // 0: game didn't start yet, 1: playing, 2: game is over
+  const { handleClick, wrongAnswerRef, answer } = useQuiz(
+    score,
+    status,
+    audios,
+    setStatus,
+    setScore,
+    timeLimit
+  ); // the logic for the quiz
 
   return (
     <>
       <MyHead />
       <Score score={score} />
       <Question setAnswer={e => (answer.current = e)} score={score} />
-      <Quiz
-        score={score}
-        setScore={setScore}
-        answer={answer}
-        audios={audios}
+      <Answers
         time={timeLimit}
-        status={status}
-        setStatus={setStatus}
+        callback={status !== 2 ? handleClick : () => null} // if playing is false, we don't want to do anything when the user clicks on an answer
+        score={wrongAnswerRef.current ? -1 : score} // only if the user chose the wrong answer, re-mount the answers with score of -1 (which means: no animation, simple background). but if the user lost because of time, we don't want to re-mount the answers (stay red). score (the key) will not update.
+        resObj={{
+          correct: wrongAnswerRef.current ? answer.current : null,
+          wrong: wrongAnswerRef.current ? wrongAnswerRef.current : null,
+        }}
       />
+      {status === 2 && (
+        <GameOver
+          playAgain={() => {
+            setStatus(0);
+            setScore(0);
+            audios.current.bad && audios.current.bad.pause();
+            if (audios.current.bad) {
+              audios.current.bad.currentTime = 0; // reset the audio
+            }
+            wrongAnswerRef.current = null;
+          }}
+        />
+      )}
       <Fabs
         muteCallback={handleMute}
         time={timeLimit}
